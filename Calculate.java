eeb8f28ceb4fdfd0344eab58dbd0a011520a724a
Миргородский Роман Викторович <mirgorodskiy_rv@UFNP.local>
2018-11-07 12:17:07 +0700

    create 3 Calculate

--- Calculate.java
+++ Calculate.java
@@ -5,8 +5,14 @@ public class Calculate {
 		int second = Integer.valueOf (arg[1]);
 		int summ = firs+second;
 		int raznica = firs-second;
-		
+		int proiz = firs*second;
+		float chastnoe =   (float)firs/ (float)second;
+		System.out.println ("Введены числа = "+firs+"  "+second);
 		System.out.println ("Сумма чисел = "+summ);
 		System.out.println ("Разница чисел = "+raznica);
+		System.out.println ("Произведение чисел = "+proiz);
+		System.out.print ("Частное чисел = ");
+		System.out.printf ("%.2f", chastnoe);
+		
 	}
 }
