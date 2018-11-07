97a051d57b5f4e23daca788f31af17887e58641d
Миргородский Роман Викторович <mirgorodskiy_rv@UFNP.local>
2018-11-07 10:51:15 +0700

    create2 Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,12 @@
 public class Calculate {
 	public static void main (String[] arg) {
 		System.out.println("Calculate...");
+		int firs = Integer.valueOf (arg[0]);
+		int second = Integer.valueOf (arg[1]);
+		int summ = firs+second;
+		int raznica = firs-second;
 		
+		System.out.println ("Сумма чисел = "+summ);
+		System.out.println ("Разница чисел = "+raznica);
 	}
 }
